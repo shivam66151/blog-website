e61eae062215f893bc65902145a5fe3c1ef3c7df
shivam66151 <sharmashivam66151@gmail.com>
2022-05-29 22:43:55 +0530

    topbar creation

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import TopBar from './topbar/TopBar'
 
 function App() {
   return (
-    <div className="App">
+    <>
     <TopBar />
-    </div>
+    
+    </>
   );
 }
 
--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -3,6 +3,15 @@ import './topbar.css'
 
 export default function TopBar() {
   return (
-    <div className='top'>TopBar</div>
+    <div className='top'>
+        <div className='topLeft'>
+            <i className="fab fa-facebook square"></i>
+            <i className="fab fa-twitter square"></i>
+            <i className="fab fa-pinterest square"></i>
+            <i className="fab fa-instagram square"></i>
+        </div>
+        <div className='topCenter'>Center</div>
+        <div className='topRight'>2</div>
+    </div>
   )
 }
